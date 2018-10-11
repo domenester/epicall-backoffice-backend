9ed071db771a79f115f5c5809a4b730307ce736e
domenester <diogodomene@gmail.com>
2018-10-11 16:20:25 -0300

    Adding video mocks

--- src/services/record/mocks.ts
+++ src/services/record/mocks.ts
@@ -1,6 +1,6 @@
 export default () => ([
   {
-    id: "record1",
+    id: "audio1",
     participants: [
       {
         id: "e4a45df3222c8117121681c58d1a2e59",
@@ -14,5 +14,21 @@ export default () => ([
     // 2018-09-29T02:06:39.518Z
     date: new Date(1538186799518),
     url: `${process.env.NODE_URL}/public/audios/sample.mp3`
+  },
+  {
+    id: "video1",
+    participants: [
+      {
+        id: "e4a45df3222c8117121681c58d1a2e59",
+        name: "Daniel"
+      }
+    ],
+    type: "video",
+    duration: "00:05:13",
+    ext: "123",
+    // Fri Sep 28 2018 23:06:39 GMT-0300 (Brasilia Standard Time)
+    // 2018-09-29T02:06:39.518Z
+    date: new Date(1538186799518),
+    url: `${process.env.NODE_URL}/public/audios/sample.mp4`
   }
 ]);
