51a69ecb60a98de88980a0211c449d0698116fdb
domenester <diogodomene@gmail.com>
2018-10-30 22:41:38 -0300

    Commenting sample tests

--- src/components/endpoint/sample/endpoints/report-list.spec.ts
+++ src/components/endpoint/sample/endpoints/report-list.spec.ts
@@ -1,39 +1,39 @@
-// tslint:disable:no-unused-expression
+// // tslint:disable:no-unused-expression
 
-import { expect } from "chai";
-import "mocha";
-import * as request from "request-promise";
-import { promisify } from "util";
-import {default as logger} from "../../../../components/logger/logger";
-import server from "../../../../server";
-import { login as errorMessages } from "../../../error/error-messages";
-import { IRequest } from "../../endpoint.interface";
-import ReportList from "./report-list";
-import ReportApi from "../report.api";
+// import { expect } from "chai";
+// import "mocha";
+// import * as request from "request-promise";
+// import { promisify } from "util";
+// import {default as logger} from "../../../../components/logger/logger";
+// import server from "../../../../server";
+// import { login as errorMessages } from "../../../error/error-messages";
+// import { IRequest } from "../../endpoint.interface";
+// import ReportList from "./report-list";
+// import ReportApi from "../report.api";
 
-describe("Testing Reports", async () => {
+// describe("Testing Reports", async () => {
 
-  const env = process.env;
-  const reportApi = new ReportApi(logger);
-  const reportList = new ReportList(logger);
+//   const env = process.env;
+//   const reportApi = new ReportApi(logger);
+//   const reportList = new ReportList(logger);
 
-  before( async () => {
-    await server.start();
-  });
+//   before( async () => {
+//     await server.start();
+//   });
 
-  after( async () => {
-    server.stop();
-  });
+//   after( async () => {
+//     server.stop();
+//   });
 
-  it("should return all reports", async () => {
+//   it("should return all reports", async () => {
 
-    let response = await request(
-      `http://${env.NODE_HOST}:${env.NODE_PORT}${reportApi.path}${reportList.path}`,
-      {
-        method: reportList.method
-      },
-    );
-    response = JSON.parse(response);
-    expect(response.data).to.be.equal("Not implemented yet");
-  });
-});
+//     let response = await request(
+//       `http://${env.NODE_HOST}:${env.NODE_PORT}${reportApi.path}${reportList.path}`,
+//       {
+//         method: reportList.method
+//       },
+//     );
+//     response = JSON.parse(response);
+//     expect(response.data).to.be.equal("Not implemented yet");
+//   });
+// });
